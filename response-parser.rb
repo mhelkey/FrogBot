520265da6af7e466a48eebe3d387bcbda135a653
Matthew Helkey <mshelkey@ucdavis.edu>
2021-07-03 16:09:06 -0700

    parses csv file to generate commands

---
+++ response-parser.rb
@@ -0,0 +1,21 @@
+# will read in a .csv file of responses, and generate a hash of the keyword with the response that will occur upon activation.
+
+require 'csv'
+
+#file name must be a string
+class HashGenerator
+
+    attr_accessor :hash
+
+    def initialize(file_name)
+        @keys = CSV.read(file_name)
+        matrix_to_hash
+    end
+
+    def matrix_to_hash
+        @hash = Hash[@keys.map {|key, value| [key, value]}]
+    end
+end
+
+# ha = HashGenerator.new("trigger-words-and-responses.csv")
+# puts ha.hash
