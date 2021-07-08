11bbfcf8856e4fc7c1e1413c7cd1c3efcbf42d22
Matthew Helkey <mshelkey@ucdavis.edu>
2021-07-07 21:59:10 -0700

    change file pathnames to satisfy daemon requirements

--- bot_initializer.rb
+++ bot_initializer.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
-require './environment.rb'
-require './hash_generator.rb'
+require '/home/mhelkey/bots/Ruby-Discord-Bot/environment.rb'
+require '/home/mhelkey/bots/Ruby-Discord-Bot/hash_generator.rb'
 
 class DeployBot
 
@@ -8,7 +8,7 @@ class DeployBot
         @my_bot = Discordrb::Bot.new token: ENV["DISCORD_TOKEN"], client_id: ENV["APPLICATION_ID"]
         @my_bot.run true
 
-        command_hash = HashGenerator.new("triggers_and_responses.csv")
+        command_hash = HashGenerator.new("/home/mhelkey/bots/Ruby-Discord-Bot/triggers_and_responses.csv")
 
         @my_bot.message content: /[~]\w+/ do |event|
             trigger_string = event.content[/[~]\w+/]
